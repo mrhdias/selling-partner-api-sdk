fb985b3126e567ad8276d1e5e1a33796b4dafd5d
Henrique Dias <mrhdias@gmail.com>
2023-05-21 17:40:44 +0100

    Update bindparam_test.go

--- pkg/runtime/bindparam_test.go
+++ pkg/runtime/bindparam_test.go
@@ -21,7 +21,7 @@ import (
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 
-	"github.com/amzapi/selling-partner-api-sdk/pkg/types"
+	"github.com/mrhdias/selling-partner-api-sdk/pkg/types"
 )
 
 func TestSplitParameter(t *testing.T) {
